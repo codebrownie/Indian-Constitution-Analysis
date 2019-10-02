27eb62a7d4eb0df21669d5c1b540c57d51811b66
codebrownie <gayathrydasikaresearch@gmail.com>
2019-10-03 00:37:04 +0800

    Download script

--- constitution_pdf.py
+++ constitution_pdf.py
@@ -1,1 +1,17 @@
-import requests 
+"""
+Subprocess calls that will download latest constitution data from the website. 
+"""
+import subprocess
+import os
+try:
+    os.mkdir('./dataset/')
+except:
+    print('Dataset folder exists.')
+
+print('Downloading files from constitution website.')
+subprocess.call(['curl','-o','./dataset/coi_preface.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi_preface.pdf'])
+subprocess.call(['curl','-o','./dataset/coi_contents.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi_contents.pdf'])
+subprocess.call(['curl','-o','./dataset/coi_part_full.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi_part_full.pdf'])
+subprocess.call(['curl','-o','./dataset/coi-eng-schedules_1-12.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi-eng-schedules_1-12.pdf'])
+subprocess.call(['curl','-o','./dataset/coi_appendix.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi_appendix.pdf'])
+subprocess.call(['curl','-o','./dataset/coi-eng-index.pdf','https://www.india.gov.in/sites/upload_files/npi/files/coi-eng-index.pdf'])
